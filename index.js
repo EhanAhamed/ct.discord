dc596bbb8b8cec4f3a54c5f2f7de0fe667cca118
Ehan <ehanahamed@ehan.dev>
2022-07-22 15:36:25 -0400

    📦 Bundled Changes AGAIN

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
  * Discord.js RPC Extension v4.0.1 (https://github.com/discordjs/RPC/)
  * Copyright (c) 2022 devsnek (https://github.com/discordjs/RPC/graphs/contributors)
  * Licensed under MIT (https://github.com/discordjs/RPC/blob/master/LICENSE)
- * 
+ *
  * Modified by Ehan Ahamed and contributors (https://github.com/EhanAhamed/ct.discord/graphs/contributors)
- * Modified for use in ct.js (https://github.com/ct-js/ct-js)
+ * Modified for use in ct.js (https://github.com/ct-js/ct-js),
+ * through the ct.js module, ct.discord (https://github.com/EhanAhamed/ct.discord)
  * Modified Source Code is Licensed under MIT (https://github.com/EhanAhamed/ct.discord/blob/main/LICENSE.txt)
  */
 
@@ -191,9 +192,10 @@ constants.RelationshipTypes = {
  * Discord.js RPC Extension v4.0.1 (https://github.com/discordjs/RPC/)
  * Copyright (c) 2022 devsnek (https://github.com/discordjs/RPC/graphs/contributors)
  * Licensed under MIT (https://github.com/discordjs/RPC/blob/master/LICENSE)
- * 
+ *
  * Modified by Ehan Ahamed and contributors (https://github.com/EhanAhamed/ct.discord/graphs/contributors)
- * Modified for use in ct.js (https://github.com/ct-js/ct-js)
+ * Modified for use in ct.js (https://github.com/ct-js/ct-js),
+ * through the ct.js module, ct.discord (https://github.com/EhanAhamed/ct.discord)
  * Modified Source Code is Licensed under MIT (https://github.com/EhanAhamed/ct.discord/blob/main/LICENSE.txt)
  */
 
@@ -253,9 +255,10 @@ util = {
  * Discord.js RPC Extension v4.0.1 (https://github.com/discordjs/RPC/)
  * Copyright (c) 2022 devsnek (https://github.com/discordjs/RPC/graphs/contributors)
  * Licensed under MIT (https://github.com/discordjs/RPC/blob/master/LICENSE)
- * 
+ *
  * Modified by Ehan Ahamed and contributors (https://github.com/EhanAhamed/ct.discord/graphs/contributors)
- * Modified for use in ct.js (https://github.com/ct-js/ct-js)
+ * Modified for use in ct.js (https://github.com/ct-js/ct-js),
+ * through the ct.js module, ct.discord (https://github.com/EhanAhamed/ct.discord)
  * Modified Source Code is Licensed under MIT (https://github.com/EhanAhamed/ct.discord/blob/main/LICENSE.txt)
  */
 
@@ -442,9 +445,10 @@ ipc.decode = decode;
  * Discord.js RPC Extension v4.0.1 (https://github.com/discordjs/RPC/)
  * Copyright (c) 2022 devsnek (https://github.com/discordjs/RPC/graphs/contributors)
  * Licensed under MIT (https://github.com/discordjs/RPC/blob/master/LICENSE)
- * 
+ *
  * Modified by Ehan Ahamed and contributors (https://github.com/EhanAhamed/ct.discord/graphs/contributors)
- * Modified for use in ct.js (https://github.com/ct-js/ct-js)
+ * Modified for use in ct.js (https://github.com/ct-js/ct-js),
+ * through the ct.js module, ct.discord (https://github.com/EhanAhamed/ct.discord)
  * Modified Source Code is Licensed under MIT (https://github.com/EhanAhamed/ct.discord/blob/main/LICENSE.txt)
  */
 
@@ -529,9 +533,10 @@ class WebSocketTransport extends EventEmitter {
  * Discord.js RPC Extension v4.0.1 (https://github.com/discordjs/RPC/)
  * Copyright (c) 2022 devsnek (https://github.com/discordjs/RPC/graphs/contributors)
  * Licensed under MIT (https://github.com/discordjs/RPC/blob/master/LICENSE)
- * 
+ *
  * Modified by Ehan Ahamed and contributors (https://github.com/EhanAhamed/ct.discord/graphs/contributors)
- * Modified for use in ct.js (https://github.com/ct-js/ct-js)
+ * Modified for use in ct.js (https://github.com/ct-js/ct-js),
+ * through the ct.js module, ct.discord (https://github.com/EhanAhamed/ct.discord)
  * Modified Source Code is Licensed under MIT (https://github.com/EhanAhamed/ct.discord/blob/main/LICENSE.txt)
  */
 
@@ -547,9 +552,10 @@ transports = {
  * Discord.js RPC Extension v4.0.1 (https://github.com/discordjs/RPC/)
  * Copyright (c) 2022 devsnek (https://github.com/discordjs/RPC/graphs/contributors)
  * Licensed under MIT (https://github.com/discordjs/RPC/blob/master/LICENSE)
- * 
+ *
  * Modified by Ehan Ahamed and contributors (https://github.com/EhanAhamed/ct.discord/graphs/contributors)
- * Modified for use in ct.js (https://github.com/ct-js/ct-js)
+ * Modified for use in ct.js (https://github.com/ct-js/ct-js),
+ * through the ct.js module, ct.discord (https://github.com/EhanAhamed/ct.discord)
  * Modified Source Code is Licensed under MIT (https://github.com/EhanAhamed/ct.discord/blob/main/LICENSE.txt)
  */
 
