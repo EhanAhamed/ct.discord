d47ba233dd0ad243e800839bbbc7691a30d68011
Ehan <ehanahamed@ehan.dev>
2022-07-22 14:47:17 -0400

    📝 Made Copyright & License More Clear AGAIN

--- src/discord-rpc/util.js
+++ src/discord-rpc/util.js
@@ -2,9 +2,10 @@
  * Discord.js RPC Extension v4.0.1 (https://github.com/discordjs/RPC/)
  * Copyright (c) 2022 devsnek (https://github.com/discordjs/RPC/graphs/contributors)
  * Licensed under MIT (https://github.com/discordjs/RPC/blob/master/LICENSE)
- * 
+ *
  * Modified by Ehan Ahamed and contributors (https://github.com/EhanAhamed/ct.discord/graphs/contributors)
- * Modified for use in ct.js (https://github.com/ct-js/ct-js)
+ * Modified for use in ct.js (https://github.com/ct-js/ct-js),
+ * through the ct.js module, ct.discord (https://github.com/EhanAhamed/ct.discord)
  * Modified Source Code is Licensed under MIT (https://github.com/EhanAhamed/ct.discord/blob/main/LICENSE.txt)
  */
 
